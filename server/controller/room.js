f89d818406b4ba4b74ac29ba832d137d45d677f8
a1245582339 <1245582339@qq.com>
2019-02-27 18:09:57 +0800

    管理端查询房间丢数据bug

--- server/controller/room.js
+++ server/controller/room.js
@@ -5,19 +5,32 @@ exports.getRoom = async ctx => {
     let whereStr = Object.keys(query).reduce((total, curr) => {
         return [...total, `room.${curr}="${query[curr]}"`]
     }, []).join(' and ')
-    let $selectRoom = `select room.*,user.name,user.nick_name,user.tel,user.role,user.isDel from room left join user on user.room_id=room.id and user.isDel=0 ${whereStr ? 'where' + whereStr : ''}order by room.id` 
+    let $selectRoom = `select room.*,user.name,user.nick_name,user.tel,user.role,user.isDel from room left join user on user.room_id=room.id and user.isDel=0 ${whereStr ? 'and' + whereStr : ''} order by room.id` 
     console.log($selectRoom)
     await model.operateSql($selectRoom).then(res => {
-        res = res.filter(item => {
-            return item.role != 0 && item.role != 1
-        })
-        
+        // res = res.filter(item => {
+        //     return item.role != 0 && item.role != 1
+        // })
+        res = res.reduce((total, curr) => {
+            if (total.find(item => curr.id === item.id)) {
+                return total
+            }
+            let item 
+            if (curr.role === 0 || curr.role === 1 ) {
+                const { id, room_num, building, area } = curr
+                item = { id, room_num, building, area }
+            } else {
+                item = curr
+            }
+            return [...total, item]
+        }, [])
         ctx.body =  {
             code: 20000,
             msg: '房间信息',
             data: res
         }
     }).catch(err => {
+        throw err
         ctx.body = {
             code: -1,
             msg: '查询失败'
